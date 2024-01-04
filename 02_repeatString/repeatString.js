89c5e2fdfa00e5d5d545171ca392be1c558727f9
Caroline <caroline.fonseca0001@gmail.com>
2024-01-04 16:20:17 -0500

    Update all work on 01 - 04

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+// let string = "hey";
+// const repeatString = function (string, num) {
+//     i = 0; i++;
+//   if (i===3) {
+// return ${string}*num
+//   }
 
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const word = "hello";
 
+// const reverseString = function (string) {
+//   return word.split("").reverse().join("");
+// };
+
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
 };
 
+// const reverseString = function (string) {
+//   let str = "";
+//   let result = str.split("");
+//   let text = result.join;
+//   return text.reverse;
+// };
+// reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
