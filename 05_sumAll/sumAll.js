e7b168b955c2c9e974eb80083833b358a92735c8
Caroline <caroline.fonseca0001@gmail.com>
2024-01-05 15:26:42 -0500

    Work from Jan 4 and 5 on sumAll and leapYears functions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+// const sumAll = function (start, end) {
+//   let sum = 0;
+//   for (let i = 1; i <= 4; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// };
 
+// const sumAll = function (start, end) {
+//   let sum = 0;
+//   for (let i = 123; i >= 1; i--) {
+//     sum += i;
+//   }
+//   return sum;
+// };
+const sumAll = function (start, end) {
+  let sum = 0;
+  for (let i = 4; i >= -10; i++) {
+    sum += i;
+  }
+  if (sum < 0) {
+    return "ERROR";
+  } else {
+    return sum;
+  }
+  console.log(sum);
 };
 
 // Do not edit below this line
